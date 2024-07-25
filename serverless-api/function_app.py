b12f7a56c133b9a05c6a6b50dc9d994bea7b5e35
aureltest <aurelien.testelin@hotmail.fr>
2024-07-26 01:31:29 +0200

    test

--- serverless-api/function_app.py
+++ serverless-api/function_app.py
@@ -11,6 +11,8 @@ import time
 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+with open("all_article_ids.pkl", "rb") as f:
+    all_article_ids = pickle.load(f)
 
 
 @app.function_name(name="httpTrigger")
@@ -63,18 +65,16 @@ def recommender_function(
         # articles_emb = load_pickle_file(embeddingFile)
         svd_model = load_pickle_file(svdModel)
 
-        # all_article_ids = list(range(articles_emb.shape[0]))
-        with open("all_article_ids.pkl", "rb") as f:
-            all_article_ids = pickle.load(f)
+        # # all_article_ids = list(range(articles_emb.shape[0]))
 
-        top_recommended = svd_function(user_id, all_article_ids, svd_model, n=5)
+        # top_recommended = svd_function(user_id, all_article_ids, svd_model, n=5)
 
         # top_recommended = hybrid_recommendation(
         #     user_id_int, articles_emb, ratings, all_article_ids, svd_model
         # )
 
         return func.HttpResponse(
-            body=f"For user_id: {user_id}, recommandations: {top_recommended}",
+            body=f"For user_id: {user_id}",
             status_code=200,
         )
 
@@ -90,79 +90,79 @@ def load_pickle_file(file_stream):
     return pickle.loads(file_content)
 
 
-def load_and_filter_data(file_stream, user_id):
-    data = load_pickle_file(file_stream)
-    filtered_data = data[data["user_id"] == user_id]
-    return filtered_data
+# def load_and_filter_data(file_stream, user_id):
+#     data = load_pickle_file(file_stream)
+#     filtered_data = data[data["user_id"] == user_id]
+#     return filtered_data
 
 
-def get_weighted_average_embeddings(user_articles, user_click_counts, articles_emb):
-    """
-    Cette fonction prend les IDs des articles, les comptes de clics, et les embeddings des articles,
-    et retourne un embedding moyen pondéré basé sur le nombre de fois qu'un utilisateur a interagi
-    avec chaque article.
+# def get_weighted_average_embeddings(user_articles, user_click_counts, articles_emb):
+#     """
+#     Cette fonction prend les IDs des articles, les comptes de clics, et les embeddings des articles,
+#     et retourne un embedding moyen pondéré basé sur le nombre de fois qu'un utilisateur a interagi
+#     avec chaque article.
 
-    Parameters:
-    - user_articles (list of int): Liste des IDs des articles avec lesquels un utilisateur a interagi.
-    - user_click_counts (list of int): Liste du nombre de fois qu'un utilisateur a interagi avec chaque article.
-    - articles_emb (array-like): Embeddings des articles.
+#     Parameters:
+#     - user_articles (list of int): Liste des IDs des articles avec lesquels un utilisateur a interagi.
+#     - user_click_counts (list of int): Liste du nombre de fois qu'un utilisateur a interagi avec chaque article.
+#     - articles_emb (array-like): Embeddings des articles.
 
-    Returns:
-    - array-like: L'embedding moyen pondéré des articles interactés par l'utilisateur.
-    """
-    total_weight = sum(user_click_counts)
-    weighted_average_embedding = np.zeros_like(articles_emb[0])
-    for article_id, click_count in zip(user_articles, user_click_counts):
-        weighted_average_embedding += articles_emb[article_id] * (
-            click_count / total_weight
-        )
-    return weighted_average_embedding
+#     Returns:
+#     - array-like: L'embedding moyen pondéré des articles interactés par l'utilisateur.
+#     """
+#     total_weight = sum(user_click_counts)
+#     weighted_average_embedding = np.zeros_like(articles_emb[0])
+#     for article_id, click_count in zip(user_articles, user_click_counts):
+#         weighted_average_embedding += articles_emb[article_id] * (
+#             click_count / total_weight
+#         )
+#     return weighted_average_embedding
 
 
-def getnArticles(user_id, articles_emb, df_cliks, n=5):
-    """
-    Retourne une liste de n articles recommandés pour un utilisateur spécifié,
-    basée sur la similarité des embeddings des articles pondéré par le nombre de click de l'utilisateur.
-    Si l'utilisateur n'a lu aucun article, la fonction retourne les n articles les plus populaires.
+# def getnArticles(user_id, articles_emb, df_cliks, n=5):
+#     """
+#     Retourne une liste de n articles recommandés pour un utilisateur spécifié,
+#     basée sur la similarité des embeddings des articles pondéré par le nombre de click de l'utilisateur.
+#     Si l'utilisateur n'a lu aucun article, la fonction retourne les n articles les plus populaires.
 
-    Parameters:
-    userId (int): ID de l'utilisateur pour lequel les recommandations doivent être générées.
-    n (int): Nombre d'articles recommandés à récupérer.
-    articles_emb (array-like): Tableau 2D où chaque sous-liste
-                                représente l'embedding d'un article.
-    df_cliks (DataFrame): Un DataFrame pandas contenant les données de clics des
-                          utilisateurs.
+#     Parameters:
+#     userId (int): ID de l'utilisateur pour lequel les recommandations doivent être générées.
+#     n (int): Nombre d'articles recommandés à récupérer.
+#     articles_emb (array-like): Tableau 2D où chaque sous-liste
+#                                 représente l'embedding d'un article.
+#     df_cliks (DataFrame): Un DataFrame pandas contenant les données de clics des
+#                           utilisateurs.
 
-    Return:
-    list of int: Liste des IDs des n articles recommandés.
-    """
-    user_articles = df_cliks.loc[df_cliks["user_id"] == user_id]["article_id"].tolist()
-    logging.info(print(user_articles))
-    user_click_counts = df_cliks.loc[df_cliks["user_id"] == user_id][
-        "article_user_clicks"
-    ].tolist()
+#     Return:
+#     list of int: Liste des IDs des n articles recommandés.
+#     """
+#     user_articles = df_cliks.loc[df_cliks["user_id"] == user_id]["article_id"].tolist()
+#     logging.info(print(user_articles))
+#     user_click_counts = df_cliks.loc[df_cliks["user_id"] == user_id][
+#         "article_user_clicks"
+#     ].tolist()
 
-    if not user_articles:
-        print(f"User {user_id} has not read any articles yet!")
-        return
+#     if not user_articles:
+#         print(f"User {user_id} has not read any articles yet!")
+#         return
 
-    weighted_average_embedding = get_weighted_average_embeddings(
-        user_articles, user_click_counts, articles_emb
-    )
-    unread_articles = list(set(range(len(articles_emb))) - set(user_articles))
-    cos_distance = distance.cdist(
-        [weighted_average_embedding], articles_emb[unread_articles], "cosine"
-    )[0]
-    similarity_scores = 1 - cos_distance
+#     weighted_average_embedding = get_weighted_average_embeddings(
+#         user_articles, user_click_counts, articles_emb
+#     )
+#     unread_articles = list(set(range(len(articles_emb))) - set(user_articles))
+#     cos_distance = distance.cdist(
+#         [weighted_average_embedding], articles_emb[unread_articles], "cosine"
+#     )[0]
+#     similarity_scores = 1 - cos_distance
 
-    recommended_indices = np.argsort(similarity_scores)[::-1][
-        :n
-    ]  # Tri décroissant pour avoir les scores les plus élevés en premier
-    recommended_articles_scores = [
-        (unread_articles[i], similarity_scores[i]) for i in recommended_indices
-    ]
+#     recommended_indices = np.argsort(similarity_scores)[::-1][
+#         :n
+#     ]  # Tri décroissant pour avoir les scores les plus élevés en premier
+#     recommended_articles_scores = [
+#         (unread_articles[i], similarity_scores[i]) for i in recommended_indices
+#     ]
 
-    return recommended_articles_scores
+#     return recommended_articles_scores
 
 
 def svd_function(user_id, all_article_ids, algo, n=5):
@@ -188,39 +188,39 @@ def svd_function(user_id, all_article_ids, algo, n=5):
     return predictions[:n]
 
 
-def hybrid_recommendation(
-    user_id, articles_emb, df_clicks, all_article_ids, algo, n=5, w=0.5
-):
-    """
-    Retourne une liste d'articles recommandés basée sur une combinaison linéaire des scores
-    de similarité de CBF et SVD++.
-    """
+# def hybrid_recommendation(
+#     user_id, articles_emb, df_clicks, all_article_ids, algo, n=5, w=0.5
+# ):
+#     """
+#     Retourne une liste d'articles recommandés basée sur une combinaison linéaire des scores
+#     de similarité de CBF et SVD++.
+#     """
 
-    # Obtenir les recommandations de CBF
-    cbf_recommendations = getnArticles(user_id, articles_emb, df_clicks)
-    print(f"CBF recommandations : \n {cbf_recommendations}")
+#     # Obtenir les recommandations de CBF
+#     cbf_recommendations = getnArticles(user_id, articles_emb, df_clicks)
+#     print(f"CBF recommandations : \n {cbf_recommendations}")
 
-    # Obtenir les recommandations de SVD++
-    svd_recommendations = svd_function(user_id, all_article_ids, algo, n=10)
-    print(f"SVD++ recommandations : \n {svd_recommendations}")
+#     # Obtenir les recommandations de SVD++
+#     svd_recommendations = svd_function(user_id, all_article_ids, algo, n=10)
+#     print(f"SVD++ recommandations : \n {svd_recommendations}")
 
-    # Créer un dictionnaire pour stocker les scores combinés
-    combined_scores = {}
-    if cbf_recommendations:
-        for article_id, score in cbf_recommendations:
-            combined_scores[article_id] = w * score
+#     # Créer un dictionnaire pour stocker les scores combinés
+#     combined_scores = {}
+#     if cbf_recommendations:
+#         for article_id, score in cbf_recommendations:
+#             combined_scores[article_id] = w * score
 
-    for article_id, score in svd_recommendations:
-        if article_id in combined_scores:
-            combined_scores[article_id] += (1 - w) * score
-        else:
-            combined_scores[article_id] = (1 - w) * score
+#     for article_id, score in svd_recommendations:
+#         if article_id in combined_scores:
+#             combined_scores[article_id] += (1 - w) * score
+#         else:
+#             combined_scores[article_id] = (1 - w) * score
 
-    # Trier les articles par score combiné de manière décroissante
-    recommended_articles = sorted(
-        combined_scores.items(), key=lambda x: x[1], reverse=True
-    )
-    top_recommended = recommended_articles[:n]
-    print(f"Hybrid recommandations : \n {top_recommended}")
+#     # Trier les articles par score combiné de manière décroissante
+#     recommended_articles = sorted(
+#         combined_scores.items(), key=lambda x: x[1], reverse=True
+#     )
+#     top_recommended = recommended_articles[:n]
+#     print(f"Hybrid recommandations : \n {top_recommended}")
 
-    return top_recommended
+#     return top_recommended
